85acd1a031a6ab9a65ac3b6c770ae3f9051f68b4
Mariana <maricotagc@gmail.com>
2020-01-09 00:09:08 +0100

    First project

---
+++ tests/ajaxClickTest.js
@@ -0,0 +1,15 @@
+describe('Test that the button is clickable once the Ajax loader completes loading', function() {
+
+    it('Attempt to click the ASAP button', function(done) {
+        browser.url('/Ajax-Loader/index.html');
+        browser.click('#button1');
+    });
+
+    it('Attempt to click the ASAP button after 7 seconds', function(done) {
+        browser.url('/Ajax-Loader/index.html');
+        this.timeout(20000);
+        browser.pause(7000);
+        browser.click('#button1');
+        browser.pause(7000);
+        });
+});
